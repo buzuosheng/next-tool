648c95af8174cbd1f30043213b6f0c4428ea3da3
wangqi <1015350043@qq.com>
2021-09-11 11:32:48 +0800

    getHeaders

--- pages/ip.tsx
+++ pages/ip.tsx
@@ -3,16 +3,16 @@ import App from '../components/App'
 import { GetServerSidePropsContext } from 'next'
 import axios from 'axios'
 
-const Ip = ({ realIp, headers }) => {
-  console.log(realIp)
+const Ip = (props) => {
+  console.log(props.realIp)
   return (
     <App
       title="IP地址查询"
       description="在线查询IP地址信息"
       keywords="ip, ip查询, 工具, 在线工具, 前端, 程序员, 武器库"
     >
-      {realIp}
-      {headers}
+      {props.realIp}
+      {props.headers}
       {/* <IpSearch initIp={realIp} /> */}
     </App>
   )
