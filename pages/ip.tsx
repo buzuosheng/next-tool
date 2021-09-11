3cbcefd8d4717149a1872af27cba86c6cd940cb8
wangqi <1015350043@qq.com>
2021-09-11 11:17:29 +0800

    ip

--- pages/ip.tsx
+++ pages/ip.tsx
@@ -3,24 +3,28 @@ import App from '../components/App'
 import { GetServerSidePropsContext } from 'next'
 import axios from 'axios'
 
-const Ip = ({ initIp }) => (
-  <App
-    title="IP地址查询"
-    description="在线查询IP地址信息"
-    keywords="ip, ip查询, 工具, 在线工具, 前端, 程序员, 武器库"
-  >
-    <IpSearch initIp={initIp} />
-  </App>
-)
+const Ip = ({ realIp }) => {
+  console.log(realIp)
+  return (
+    <App
+      title="IP地址查询"
+      description="在线查询IP地址信息"
+      keywords="ip, ip查询, 工具, 在线工具, 前端, 程序员, 武器库"
+    >
+      <IpSearch initIp={realIp} />
+    </App>
+  )
+}
 
 export default Ip
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   // console.log(context.req.headers)
-  console.log(context.req.headers['X-Forwarded-For'])
-  const ip = await axios(
-    'http://ip-api.com/json/?fields=status,message,country,regionName,city,zip,isp,reverse,query&lang=zh-CN'
-  )
-  const initIp = await ip.data
-  return { props: { initIp } }
+  const realIp = context.req.headers['X-Forwarded-For']
+  console.log(realIp)
+  // const ip = await axios(
+  //   'http://ip-api.com/json/?fields=status,message,country,regionName,city,zip,isp,reverse,query&lang=zh-CN'
+  // )
+  // const initIp = await ip.data
+  return { props: { realIp } }
 }
